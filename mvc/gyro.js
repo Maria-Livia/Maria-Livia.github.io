c18be91f6132fb95e9a61a285d317fc8a0b3de71
Maria-Livia <56147315+Maria-Livia@users.noreply.github.com>
2019-10-25 13:46:09 +0300

    corect

---
+++ mvc/gyro.js
@@ -0,0 +1,21 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.10.25.0";
+
+window.addEventListener("deviceorientation", on_orientation_uab);
+window.addEventListener("devicemotion", on_motion_uab);
+
+function on_orientation_uab(e)
+{
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
+}
+
+function on_motion_uab(e)
+{
+	document.getElementById("id_acc_z").innerHTML = e.
+	DeviceMotionEvent.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML = e.
+	DeviceMotionEvent.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML = e.
+	DeviceMotionEvent.accelerationIncludingGravity.y;
+}
